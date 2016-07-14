7798d8252cc9edb10db7304208cfc2d20ff909d3
ojSeyi <1509941@rgu.ac.uk>
2016-07-14 20:35:38 +0100

    Index edit Commit

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bug Tracker</title>
+    <link rel="stylesheet" href="assets/stylesheet.css">
+    <link rel="stylesheet" href="assets/unsemantic-grid-responsive-tablet.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <meta name="viewpoint"
+          content="width=device-width,
+          initial-scale=1,
+          minimum-scale=1,
+          maximum-scale=1"/>
+</head>
 <?php include ("db_connection.php"); ?>
 <head>
     <meta charset="UTF-8">

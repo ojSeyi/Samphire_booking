5b3080318ee0a0aa6b477623b6ed129873118083
ojSeyi <1509941@rgu.ac.uk>
2016-07-14 20:34:25 +0100

    Index edit Commit

--- index.php
+++ index.php
@@ -11,7 +11,10 @@
             <Lable>Please select a facility</Lable>
             <select name="facility" size="1" required>
                 <?php
-
+                $getfacilities = "SELECT name FROM samphire_facilities";
+                $result = mysqli_query($db, $getfacilities);
+                while ($row = mysqli_fetch_array($result)) {
+                echo "<option>". $row['name'] . "</option>";
                 ?>
         </form>
     </div>

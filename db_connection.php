b7c8d27d333bd4896f44b1995984888b4cdd7fe6
ojSeyi <1509941@rgu.ac.uk>
2016-07-14 19:51:00 +0100

    Index edit Commit

---
+++ db_connection.php
@@ -0,0 +1,12 @@
+<?php
+define('db_server', "us-cdbr-azure-southcentral-f.cloudapp.net");
+define('db_username', "b508b6e557b8b9");
+define('db_password', "23ad37fd");
+define('db_name', "samphire_subsea");
+
+$db = mysqli_connect(db_server, db_username, db_password, db_name);
+
+if(!$db){
+    echo "Can't connect to Database" . mysqli_error();
+}
+?>

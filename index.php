6dea3ecfc281ab5e2b6a48a4c1def9891a38e0b2
ojSeyi <1509941@rgu.ac.uk>
2016-07-18 16:34:10 +0100

    Commit

--- index.php
+++ index.php
@@ -23,16 +23,16 @@
             <Lablel>Please select a facility</Lablel>
             <select name="facility" size="1" required>
                 <?php
-                $getfacilities = "SELECT name FROM samphire_facilities";
-                $result = mysqli_query($db, $getfacilities);
-                while ($row = mysqli_fetch_array($result))
-                echo "<option>". $row['name'] . "</option>";
+                    $getfacilities = "SELECT name FROM samphire_facilities";
+                    $result = mysqli_query($db, $getfacilities);
+                    while ($row = mysqli_fetch_array($result))
+                    echo "<option>". $row['name'] . "</option>";
                 ?>
             </select>
             <label>Meeting Date : </label>
-            <input id="meeting" type="date" value="2016-07-01"/>
+            <input id="meeting" type="date" value="2016-07-01" required/>
             <label>If you would require the facility for more than one day tick this box</label>
-            <input type='checkbox' name='enddate' value='yes'/>
+            <input type="number" name="daycount" size="1" required/>
             <input type='submit' value='submit'/>
         </form>
     </div>

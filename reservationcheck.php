06756f079a94767738773e37982c642cc1886548
ojSeyi <1509941@rgu.ac.uk>
2016-07-21 15:31:36 +0100

    Commit

--- reservationcheck.php
+++ reservationcheck.php
@@ -1,16 +1,41 @@
-<?php
-$reservation = $_POST['rsvDate'];
-$reservation = date("Y-m-d",strtotime($reservation));
-$type = $_POST['facility'];
-$noDays = $_POST['daycount'];
+<!DOCTYPE html>
+<html lang="en">
+<?php include ("db_connection.php"); ?>
+<head>
+    <meta charset="UTF-8">
+    <title>Samphire-Subsea: Facility Reservation</title>
+    <link rel="stylesheet" href="assets/stylesheet.css">
+    <link rel="stylesheet" href="assets/unsemantic-grid-responsive-tablet.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <meta name="viewpoint"
+          content="width=device-width,
+          initial-scale=1,
+          minimum-scale=1,
+          maximum-scale=1"/>
+</head>
+<head>
+    <meta charset="UTF-8">
+    <title>Samphire Subsea Facilities Booking</title>
+</head>
+<body>
+
+    <?php
+        $reservation = $_POST['rsvDate'];
+        $reservation = date("Y-m-d",strtotime($reservation));
+        $type = $_POST['facility'];
+        $noDays = $_POST['daycount'];
 
 
-if (isset($_POST['enddate']) && $_POST['enddate'] == 'yes'){
-    $meeting = "meeting";
-    $type = "date";
-    $date = "2016-07-02";
-    echo "<form method='post' action='reservationcheck.php'>";
-    echo "<input id='meeting' type='date' value='2016-07-02'/>";
-    echo "<input type='submit' value='submit'/>";
-    echo "</form>";
-}
+        if (isset($_POST['enddate']) && $_POST['enddate'] == 'yes'){
+            $meeting = "meeting";
+            $type = "date";
+            $date = "2016-07-02";
+            echo "<form method='post' action='reservationcheck.php'>";
+            echo "<input id='meeting' type='date' value='2016-07-02'/>";
+            echo "<input type='submit' value='submit'/>";
+            echo "</form>";
+        }
+    ?>
+
+</body>
+</html>

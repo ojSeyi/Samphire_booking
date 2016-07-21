08e4ec1c644d2939ba4797fd72c32fdbf077e374
ojSeyi <1509941@rgu.ac.uk>
2016-07-21 15:34:20 +0100

    Commit

--- reservationcheck.php
+++ reservationcheck.php
@@ -19,23 +19,23 @@
 </head>
 <body>
 
-    <?php
-        $reservation = $_POST['rsvDate'];
-        $reservation = date("Y-m-d",strtotime($reservation));
-        $type = $_POST['facility'];
-        $noDays = $_POST['daycount'];
+        <?php
+            $reservation = $_POST['rsvDate'];
+            $reservation = date("Y-m-d",strtotime($reservation));
+            $type = $_POST['facility'];
+            $noDays = $_POST['daycount'];
 
 
-        if (isset($_POST['enddate']) && $_POST['enddate'] == 'yes'){
-            $meeting = "meeting";
-            $type = "date";
-            $date = "2016-07-02";
-            echo "<form method='post' action='reservationcheck.php'>";
-            echo "<input id='meeting' type='date' value='2016-07-02'/>";
-            echo "<input type='submit' value='submit'/>";
-            echo "</form>";
-        }
-    ?>
+            if (isset($_POST['enddate']) && $_POST['enddate'] == 'yes'){
+                $meeting = "meeting";
+                $type = "date";
+                $date = "2016-07-02";
+                echo "<form method='post' action='reservationcheck.php'>";
+                echo "<input id='meeting' type='date' value='2016-07-02'/>";
+                echo "<input type='submit' value='submit'/>";
+                echo "</form>";
+            }
+        ?>
 
 </body>
 </html>

bccdeed09ac279d5f095f33a6b53beb97904fd22
ojSeyi <1509941@rgu.ac.uk>
2016-07-21 22:59:23 +0100

    commit

---
+++ reservationcheck.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include ("db_connection.php"); ?>
+<head>
+    <meta charset="UTF-8">
+    <title>Samphire-Subsea: Facility Reservation</title>
+    <link rel="stylesheet" href="assets/stylesheet.css">
+    <link rel="stylesheet" href="assets/unsemantic-grid-responsive-tablet.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <meta name="viewpoint"
+          content="width=device-width,
+          initial-scale=1,
+          minimum-scale=1,
+          maximum-scale=1"/>
+</head>
+<head>
+    <meta charset="UTF-8">
+    <title>Samphire Subsea Facilities Booking</title>
+</head>
+<body>
+<div id="syscon">
+    <?php
+    $startdate = $_POST['startdate'];
+    $startdate = date("Y-m-d",strtotime($enddate));
+    $enddate = $_POST['enddate'];
+    $enddate = date("Y-m-d",strtotime($enddate));
+    $type = $_POST['facility'];
+
+    echo "<div id='reservationdetails'>
+                <p>Facility: " .$type. "</p>
+                <p>Start Date: " .$startdate. "</p>
+                <p>End Date: " .$enddate. "</p>
+            </div>";
+
+
+
+    ?>
+</div>
+</body>
+</html>

97e54fb78fa2a805d90c7c833badb624d155d638
ojSeyi <1509941@rgu.ac.uk>
2016-07-22 13:50:19 +0100

    commit

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 <header>
     <img src="assets/images/logo_2016.jpg" id="logo"/>
     <div id="pagetitie"><h4>Samphire-Subsea</h4><h5>Facilities Booking System</h5></div>
-    <form id="login" method="post" action="login.php">
+    <form id="login" method="post" action="login.php"> 
         <input type="text" name="uname" id="usernamebox" placeholder="Username"/>
         <input type="password" name="pword" id="passwordbox" placeholder="Password"/>
         <input type="submit" value="Login" id="loginb"/>

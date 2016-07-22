ccabebd4f291bb1087375eedbbf522961a86fa7a
ojSeyi <1509941@rgu.ac.uk>
2016-07-22 19:50:00 +0100

    Commit

--- reservationcheck2.php
+++ reservationcheck2.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include ("db_connection.php"); ?>
+<?php session_start();
+if(is_null($_SESSION['firstname'])){
+    header('Location: index.php');
+};
+?>
 <head>
     <meta charset="UTF-8">
     <title>Samphire-Subsea: Facility Reservation</title>
@@ -18,6 +23,21 @@
     <title>Samphire Subsea Facilities Booking</title>
 </head>
 <body>
+<header>
+    <img src="assets/images/logo_2016.jpg" id="logo"/>
+    <div id="logout">
+        <form method="post" action="logout.php">
+            <label><?php echo $_SESSION['firstname'];?></label>
+            <input type="submit" name="logout" value="logout" id="logoutbutton"/>
+        </form>
+    </div>
+    <div id="pagetitle"><h4>Samphire-Subsea</h4><p>Facilities Booking System</p></div>
+</header>
+<main>
+    <section id="bannerbox">
+        <img src="assets/images/banner1.jpg" id="bannerimage"/>
+    </section>
+
      <div id="syscon">
         <?php
             session_start();
@@ -52,5 +72,7 @@
 
         ?>
     </div>
+</main>
+
 </body>
 </html>

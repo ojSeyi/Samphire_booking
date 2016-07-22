9e282816626b670d42cf6f5204e8df1935b206e1
ojSeyi <1509941@rgu.ac.uk>
2016-07-22 16:29:25 +0100

    commit

--- login.php
+++ login.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: OJ Pumping
- * Date: 22/07/2016
- * Time: 02:20
- */
+    session_start();
+
+    include ("db_connection.php");
+
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $login = "SELECT * FROM customer_login WHERE username = '$username' AND password = '$password'";
+    $result = mysqli_query($login, $db);
+
+    if (mysqli_num_rows($result) == 1){
+        $row = mysqli_fetch_row($result);
+        $user = $row['username'];
+        $_SESSION['user'] = $user;
+        header('Location: index1.php');
+    }else{
+        header('Location: index2.php');
+    }

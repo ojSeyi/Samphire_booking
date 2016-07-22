7939b1177463d228bb80e720aa0642413cf98cb9
ojSeyi <1509941@rgu.ac.uk>
2016-07-22 17:10:44 +0100

    commit

--- login.php
+++ login.php
@@ -3,18 +3,22 @@
 
     include ("db_connection.php");
 
+if(empty($_POST['username']) || empty($_POST['password'])) {
+    echo "Enter Username and Password";
+}else{
+
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     $login = "SELECT log_id FROM customer_login WHERE username = '$username' AND password = '$password'";
-    $result = mysqli_query($db,$login) or die("failed query".mysqli_error());
+    $result = mysqli_query($db, $login);
 
     if (mysqli_num_rows($result) == 1){
-        $row = mysqli_fetch_array($result);
-        $logid = $row['log_id'];
-        $user = mysqli_query($db, "SELECT firstname FROM customers WHERE log_id = '$logid'");
+        $row = mysqli_fetch_row($result);
+        $user = $row['firstname'];
         $_SESSION['firstname'] = $user;
         header('Location: index1.php');
     }else{
         header('Location: index2.php');
     }
+}

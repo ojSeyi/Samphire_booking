2e1d9f86c710d3ec340edc86d07ef3aeaea04187
ojSeyi <1509941@rgu.ac.uk>
2016-07-22 18:39:00 +0100

    Commit

--- index1.php
+++ index1.php
@@ -1,9 +1,63 @@
-<?php
-    session_start();
-    if(isset($_SESSION['firstname'])) {
-        echo "Welcome: " . $_SESSION['firstname'];
-    }else{
-        echo "not set";
-    }
-
+<!DOCTYPE html>
+<html lang="en">
+<?php include ("db_connection.php"); ?>
+<?php session_start();
+    $_SESSION['firstname'] = $firstname;
 ?>
+<head>
+    <meta charset="UTF-8">
+    <title>Samphire-Subsea: Facility Reservation</title>
+    <link rel="stylesheet" href="assets/stylesheet.css">
+    <link rel="stylesheet" href="assets/unsemantic-grid-responsive-tablet.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <meta name="viewpoint"
+          content="width=device-width,
+          initial-scale=1,
+          minimum-scale=1,
+          maximum-scale=1"/>
+</head>
+<head>
+    <meta charset="UTF-8">
+    <title>Samphire Subsea Facilities Booking</title>
+</head>
+<body>
+<header>
+    <img src="assets/images/logo_2016.jpg" id="logo"/>
+    <div id="logout">
+        <form>
+            <label><?php echo $firstname;?></label>
+            <input type="submit" name="logout" value="logout" id="logout"/>
+        </form>
+    </div>
+    <div id="pagetitle"><h4>Samphire-Subsea</h4><p>Facilities Booking System</p></div>
+</header>
+
+<main>
+    <section id="bannerbox">
+        <img src="assets/images/banner1.jpg" id="bannerimage"/>
+    </section>
+
+    <div id="syscon">
+
+        <form id="search" method="post" action="reservationcheck2.php">
+            <Label>Please select a facility</Label>
+            <select name="facility" size="1" required>
+                <?php
+                $getfacilities = "SELECT name FROM samphire_facilities";
+                $result = mysqli_query($db, $getfacilities);
+                while ($row = mysqli_fetch_array($result))
+                    echo "<option>". $row['name'] . "</option>";
+                ?>
+            </select><br><br>
+            <label>Reservation Date : </label>
+            <input id="startdate" name="startdate" type="date" value="2016-07-01"/><br><br>
+            <label>If you would require the facility for more than one day tick this box</label><br>
+            <input type="checkbox" id="enddate" name="enddate" value="yes"/><br><br><br>
+            <input type="submit" value="submit"/><br><br>
+        </form>
+
+    </div>
+</main>
+
+</body>
+</html>

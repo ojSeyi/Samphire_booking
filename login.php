d1ddc1483150efa16c9567e89c95d944b95aa751
ojSeyi <1509941@rgu.ac.uk>
2016-07-22 17:27:11 +0100

    commit

--- login.php
+++ login.php
@@ -18,7 +18,7 @@ if(empty($_POST['username']) || empty($_POST['password'])) {
         $getuserfullname = mysqli_query($db, "SELECT firstname, lastname FROM customers WHERE log_id = $userloginid");
         $userfullname = mysqli_fetch_row(getuserfullname);
         $userfirstname = $userfullname['firstname'];
-        $_SESSION['firstname'] = $userfirstname;
+        $userfirstname = $_SESSION['firstname'];
         header('Location: index1.php');
     } else {
         header('Location: index2.php');

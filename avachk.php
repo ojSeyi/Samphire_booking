6a118866e2235c8053e081f8097d7b0be85925c0
ojSeyi <1509941@rgu.ac.uk>
2016-08-01 15:20:26 +0100

    Commit

--- avachk.php
+++ avachk.php
@@ -50,9 +50,8 @@ if(is_null($_SESSION['facilities']) && ($_SESSION['startdates'])){
         echo "was good<br>";
             echo "$facilitys <br>";
             //Upgrade code to search through date range too
-            $available = "SELECT * FROM samphire_facilities WHERE f_name = '$facilitys'";
+            $available = "SELECT * FROM samphire_facilities WHERE 'f_name' = '$facilitys'";
             $result = mysqli_query($db, $result);
-            echo mysqli_num_rows($result);
             if(mysqli_num_rows($result) > 0) {
                 echo "yes";
                 $row = mysqli_fetch_array($result);

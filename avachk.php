00377bc26d4d3b099a11603e3219ad5bdd8667a5
ojSeyi <1509941@rgu.ac.uk>
2016-08-02 04:54:01 +0100

    Commit

--- avachk.php
+++ avachk.php
@@ -60,7 +60,7 @@ if(is_null($_SESSION['facilities']) && ($_SESSION['startdates'])){
                 $rows = $row['f_id'];
                 echo $rows;
                 if(is_null($enddates)){
-                    $availables = "SELECT * FROM guest_bookings WHERE f_id = '$rows' AND startdate = $startdates";
+                    $availables = "SELECT * FROM guest_bookings WHERE f_id = '$rows' AND startdate = ''$startdates'";
                     $results = mysqli_query($db, $availables);
                         if(mysqli_num_rows($results) > 0){
                             $notavailable = 1;

26116caabce1fc67485a130cdf08a60f49548bd8
ojSeyi <1509941@rgu.ac.uk>
2016-08-03 16:28:36 +0100

    Commit

--- avachk.php
+++ avachk.php
@@ -119,16 +119,15 @@ if(is_null($_SESSION['facilities']) && ($_SESSION['startdates'])){
                         }else{
 
                         }
-                        if(count($datesinrange) > 31){
-                            echo "<label>For booking greater than 30 days please contact the office using the information in the contact page</label>";
-                        }else{
-                            echo "<label>The following dates are unavailable: </label><br>";
-                            foreach($unavailabledates as $showdate){
+                    }
+                    if(count($datesinrange) > 31){
+                        echo "<label>For booking greater than 30 days please contact the office using the information in the contact page</label>";
+                    }else{
+                        echo "<label>The following dates are unavailable: </label><br>";
+                        foreach($unavailabledates as $showdate){
 
-                                echo "<label>".$showdate.", "."</label><br><br><br>";
-                            }
+                            echo "<label>".$showdate.", "."</label><br><br><br>";
                         }
-
                     }
 
 

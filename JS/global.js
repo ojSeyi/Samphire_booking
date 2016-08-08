6ef6f85dfff38985da63ce44a293349f077b2cd7
ojSeyi <1509941@rgu.ac.uk>
2016-08-08 19:16:13 +0100

    Commit

--- JS/global.js
+++ JS/global.js
@@ -3,5 +3,4 @@ var yyyy = dt.getFullYear().toString();
 var mm = (dt.getMonth()+1).toString(); // getMonth() is zero-based
 var dd  = dt.getDate().toString();
 var min = yyyy +'-'+ (mm[1]?mm:"0"+mm[0]) +'-'+ (dd[1]?dd:"0"+dd[0]); // padding
-alert(min);
 $('#startdate').prop('min',min);
--- home1.php
+++ home1.php
@@ -51,7 +51,7 @@ $currentnextdate = date('Y-m-d', ($currentdate + 1)); ?>
                 ?>
             </select><br><br>
             <label>Reservation Date : </label>
-            <input id="startdate" name="startdate" type="date" min="01-08-2016"/><br><br>
+            <input id="startdate" name="startdate" type="date"/><br><br>
             <label>If you would require the facility for more than one day tick this box</label><br>
             <input type="checkbox" id="enddate" name="enddateC" value="yes"/><br><br><br>
             <input type="submit" value="submit"/><br><br>

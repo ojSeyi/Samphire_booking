b41fd5df9c37f59c9146e3ab49d87cb700d09317
ojSeyi <1509941@rgu.ac.uk>
2016-08-08 17:44:08 +0100

    Commit

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
-
-<?php include ("db_connection.php"); ?>
-<?php session_start();
-if(isset($_SESSION['login'])){
-    header('Location: index1.php');
-};
-?>
-<?php
-$currentdate = date('d-m-Y');
-$currentnextdate = date('d-m-Y', ($currentdate + 1)); ?>
-
 <!DOCTYPE html>
 <html lang="en">
+<?php include ("db_connection.php"); ?>
+<?php session_start();
+if(is_null($_SESSION['login'])){
+    header('Location: index.php');
+};
+?>
 <head>
     <meta charset="UTF-8">
     <title>Samphire-Subsea: Facility Reservation</title>
@@ -23,15 +18,19 @@ $currentnextdate = date('d-m-Y', ($currentdate + 1)); ?>
           minimum-scale=1,
           maximum-scale=1"/>
 </head>
-
+<head>
+    <meta charset="UTF-8">
+    <title>Samphire Subsea Facilities Booking</title>
+</head>
 <body>
 <header>
     <img src="assets/images/logo_2016.jpg" id="logo"/>
     <div id="form">
+        <div id="loginerror">Please enter a valid username and password</div>
         <form method="post" action="login.php">
             <input type="text" name="username" id="usernamebox" placeholder="Username" required/>
             <input type="password" name="password" id="passwordbox" placeholder="Password" required/>
-            <input type="submit" value="Login" name="login" id="loginb"/>
+            <input type="submit" value="Login" id="loginb"/>
         </form>
     </div>
     <div id="pagetitle"><h4>Samphire-Subsea</h4><p>Facilities Booking System</p></div>
@@ -55,9 +54,9 @@ $currentnextdate = date('d-m-Y', ($currentdate + 1)); ?>
                 ?>
             </select><br><br>
             <label>Reservation Date : </label>
-            <input id="startdate" name="startdate" type="date" value="<?php$currentnextdate?>"/><br><br>
+            <input id="startdate" name="startdate" type="date" value="2016-07-01"/><br><br>
             <label>If you would require the facility for more than one day tick this box</label><br>
-            <input type="checkbox" id="enddate" name="enddateC" value="yes"/><br><br><br>
+            <input type="checkbox" id="enddateC" name="enddate" value="yes"/><br><br><br>
             <input type="submit" value="submit"/><br><br>
         </form>
 

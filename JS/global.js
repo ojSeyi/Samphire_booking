73ae1f38a90fddf56023535427107b83a52b976e
ojSeyi <1509941@rgu.ac.uk>
2016-08-08 19:19:55 +0100

    Commit

--- JS/global.js
+++ JS/global.js
@@ -4,3 +4,4 @@ var mm = (dt.getMonth()+1).toString(); // getMonth() is zero-based
 var dd  = dt.getDate().toString();
 var min = yyyy +'-'+ (mm[1]?mm:"0"+mm[0]) +'-'+ (dd[1]?dd:"0"+dd[0]); // padding
 $('#startdate').prop('min',min);
+$('#startdate').prop('value',min);

ed6af656cb5cbec967ee8cfbdb2911bc855afaeb
ojSeyi <1509941@rgu.ac.uk>
2016-08-09 18:38:09 +0100

    Commit

--- ajax/addfacilityproc.php
+++ ajax/addfacilityproc.php
@@ -5,7 +5,7 @@ echo $_POST['addfacility'];
 
     $facilityarray = array();
     $facilityarray[0] = $_SESSION['facili'];
-    $facilityarray[] = $_POST['addfacility'];
+    $facilityarray[] = $_POST['facility'];
     echo (count($facilities) > 1) ?
          '<div><label> Facilities to be reserved:  </label><br><br></div>
                         <div>

91bb23ea9b24d573d3cbefa22f0b89b44e8d6cc7
ojSeyi <1509941@rgu.ac.uk>
2016-08-09 18:05:13 +0100

    Commit

--- ajax/addfacilityproc.php
+++ ajax/addfacilityproc.php
@@ -1,5 +1,8 @@
 <?php
 
+echo $_SESSION['facili'];
+echo $_POST['addfacility'];
+
     $facilityarray = array();
     $facilityarray[0] = $_SESSION['facili'];
     $facilityarray[] = $_POST['addfacility'];

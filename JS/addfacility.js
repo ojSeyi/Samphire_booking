b2bac353e60084d9a342e0c8fd538584e0d27047
ojSeyi <1509941@rgu.ac.uk>
2016-08-09 17:02:40 +0100

    Commit

--- JS/addfacility.js
+++ JS/addfacility.js
@@ -2,7 +2,7 @@
  * Created by OJ Pumping on 09/08/2016.
  */
 $('input#facilityarray-addfacility').on('click', function(){
-    $.post('addfacilityproc.php',{addfacility:addfacility},function(facilityarray){
+    $.post('addfacilityproc.php',{facilityarray:facilityarray},function(facilityarray){
         $('#facili-facilityarray').text(facilityarray);
 
     });

040d859c41728cdd03b0c7a22ade57305a1e6fef
ojSeyi <1509941@rgu.ac.uk>
2016-08-09 19:00:43 +0100

    Commit

--- ajax/addfacilityproc.php
+++ ajax/addfacilityproc.php
@@ -6,11 +6,9 @@ echo $_POST['addfacility'];
     $facilityarray = array();
     $facilityarray[0] = $_SESSION['facili'];
     $facilityarray[] = $_POST['facility'];
-    echo (count($facilities) > 1) ? '<?php
-                                            foreach($facilityarray as $facility){
-                                            echo <td>'.$facilityarray.'</td>;
-                                            }
-                                        ?>' : '.$facilityarray[0].';
+    echo (count($facilities) > 1) ? "<?php foreach($facilityarray as $facility){
+                                            echo $facilityarray;
+                                            } ?>" : "<?php $facilityarray[0] ?>";
 
 
 foreach($facilityarray as $facility){

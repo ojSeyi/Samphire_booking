0f2f21e645f85046fbce93bf1ffe5f8dc38eda20
ojSeyi <1509941@rgu.ac.uk>
2016-08-09 01:54:08 +0100

    Commit

--- bookstate.php
+++ bookstate.php
@@ -3,7 +3,7 @@
 <?php include ("db_connection.php"); ?>
 <?php
 session_start();
-if(is_null($_SESSION['facilities']) && ($_SESSION['startdates'])){
+if(is_null($_SESSION['facili']) && ($_SESSION['start'])){
     header('Location: index.php');
 };
 ?>

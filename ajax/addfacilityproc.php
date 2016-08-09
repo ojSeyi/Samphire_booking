67abd2c6e30e801ee653a1d07bd269ac10ff087f
ojSeyi <1509941@rgu.ac.uk>
2016-08-09 17:31:30 +0100

    Commit

--- ajax/addfacilityproc.php
+++ ajax/addfacilityproc.php
@@ -1,7 +1,2 @@
 <?php
-if(isset($_POST['addfacility'])){
-    $facilityarray = array();
-    $facilityarray[0] = $_SESSION['facili'];
-    $facilityarray[] = $_POST['addfacility'];
-    echo "fuck";
-}
+echo "fuck";

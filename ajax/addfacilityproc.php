7ddcb553ca1854d58afce6af5cd8f0824f87ca69
ojSeyi <1509941@rgu.ac.uk>
2016-08-09 18:35:33 +0100

    Commit

--- ajax/addfacilityproc.php
+++ ajax/addfacilityproc.php
@@ -6,25 +6,21 @@ echo $_POST['addfacility'];
     $facilityarray = array();
     $facilityarray[0] = $_SESSION['facili'];
     $facilityarray[] = $_POST['addfacility'];
-    if(count($facilities) > 1){
-        echo '<div><label> Facilities to be reserved:  </label><br><br></div>
+    echo (count($facilities) > 1) ?
+         '<div><label> Facilities to be reserved:  </label><br><br></div>
                         <div>
                             <form>
                                 <table>
                                     <tr>
                                         <?php
                                             foreach($facilityarray as $facility){
-                                            echo <td>.$facility.</td>;
+                                            echo <td>'.$facility.'</td>;
                                             }
                                         ?>
                                     </tr>
                                 </table>
                             </form>
-                        </div>';
-    }elseif(count($facilities) == 0){
-        echo '<div><label> Facility to be reserved:  <?php echo "".$facilityarray[0]."" ?></label><br><br></div>';
+                        </div>' :
+         '<div><label> Facility to be reserved:  '.$facilityarray[0].'</label><br><br></div>';
 
-    }elseif((count($facilities) == 1)){
-        echo '<div><label> Facility to be reserved:  <?php echo "".$facilityarray[0]."" ?></label><br><br></div>';
-    }
-    echo 'fuck';
+

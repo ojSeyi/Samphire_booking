f3fb49d17201e0dc9b7b6695dea18fa2240b7422
ojSeyi <1509941@rgu.ac.uk>
2016-08-09 19:46:57 +0100

    Commit

--- addfacility.php
+++ addfacility.php
@@ -7,19 +7,19 @@ if(is_null($_POST['newfacility'])){
 
 $newfacility = $_POST['newfacility'];
 $facilityarray = array();
+$facilityarray[0] = $_SESSION['facili'];
 foreach($facilityarray as $count){
     if($count = $_POST['newfacility']){
-        header('location: bookstate.php');
+
     }else{
         if(is_null($facilityarray[0])){
             $facilityarray[0] = $_SESSION['facili'];
-            header('location: bookstate.php');
         }else {
             $facilityarray[] = $newfacility;
             $_SESSION['facilityarray'] = $facilityarray;
-            header('location: bookstate.php');
         }
     }
+    header('location: bookstate.php');
 }
 
 

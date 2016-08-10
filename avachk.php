cae66a7ba7257811157d139a1925a64236583929
ojSeyi <1509941@rgu.ac.uk>
2016-08-10 11:53:00 +0100

    Commit

--- avachk.php
+++ avachk.php
@@ -84,7 +84,7 @@ if(is_null($_SESSION['facilities']) && is_null($_SESSION['startdates'])){
                         }else{
                             $_SESSION['start'] = $startdates;
                             $_SESSION['facili'] = $facility;
-                            header('location: bookstate.php');
+                            header('location: facilitycount.php');
                         }
                 }else{
                     function createDateRangeArray($strDateFrom,$strDateTo)

c477686f602306ce63262559736df6e43150a1c2
ojSeyi <1509941@rgu.ac.uk>
2016-08-10 11:28:12 +0100

    Commit

--- bookstate.php
+++ bookstate.php
@@ -60,6 +60,7 @@ if(is_null($_SESSION['facili']) && is_null($_SESSION['start'])){
             <div id="facili">
                 <?php
                 $i = 0;
+                echo $i;
                 $facilityarray = array();
                 $facilityarray[0] = $_SESSION['facili'];
                 $newfacility = $_POST['facilityarray'];

9ae69feb5c6d8c095650425c253e2aebe03618ee
ojSeyi <1509941@rgu.ac.uk>
2016-08-10 10:40:53 +0100

    Commit

--- bookstate.php
+++ bookstate.php
@@ -60,6 +60,7 @@ if(is_null($_SESSION['facili']) && is_null($_SESSION['start'])){
             <div id="facili">
                 <?php
                 $facilityarray = array();
+                $facilityarray[] = $_SESSION['facili'];
 
                 echo "<table><tr>";
                 foreach($facilityarray as $showdate){

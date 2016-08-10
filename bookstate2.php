cbfa28932360f15d2c3fb4296c85312e09f1aec2
ojSeyi <1509941@rgu.ac.uk>
2016-08-10 12:18:18 +0100

    Commit

--- bookstate2.php
+++ bookstate2.php
@@ -73,7 +73,7 @@ if(is_null($_POST['facilityarray'])){
                         echo "<div><table><tr><td>".$_SESSION['facili']."</td></tr></table></div>";
                         echo count($facilityarray);
                     } else {
-                        $facilityarray[$_SESSION['count']] = $newfacility;
+                        $facilityarray[$_SESSION['count'] = $newfacility;
                         echo "<div><table><tr>";
                         foreach ($facilityarray as $showdate) {
                             echo "<td>" . $showdate . "</td>";

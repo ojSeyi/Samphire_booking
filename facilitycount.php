86c24d557e0540546c15bf4a0e8c216581ee7054
ojSeyi <1509941@rgu.ac.uk>
2016-08-10 12:04:55 +0100

    Commit

--- facilitycount.php
+++ facilitycount.php
@@ -1,10 +1,11 @@
 <?php
 $s = 0;
+$_SESSION['s'] = $s;
 if(isset($_POST['number'])){
-    $_SESSION['s'] = $s;
     $_SESSION['s'] = $_SESSION['s'] + $_POST['number'];
     echo 'success';
 }else{
     $_SESSION['s'] = $s;
 }
+
 header('location: bookstate.php');

c37c10f71d420cb8d75fbb7608fec1f11f795c58
ojSeyi <1509941@rgu.ac.uk>
2016-08-10 12:24:49 +0100

    Commit

--- bookstate2.php
+++ bookstate2.php
@@ -60,13 +60,12 @@ if(is_null($_POST['facilityarray'])){
             <div id="facili">
                 <?php
                 $_SESSION['count'] = $_SESSION['count'] + 1;
-                $s = $_SESSION['count'];
-                echo $s;
+                echo $_SESSION['count'];
 
                 $facilityarray = array();
                 $facilityarray[0] = $_SESSION['facili'];
                 $newfacility = $_POST['facilityarray'];
-                $facilityarray[$s] = $newfacility;
+                $facilityarray[$_SESSION['count']] = $newfacility;
                     if ($_SESSION['facili'] = $_POST['facilityarray']) {
                         echo "<div><label>You cannot select the same facility</label></div>";
                         echo "<div><table><tr><td>".$_SESSION['facili']."</td></tr></table></div>";

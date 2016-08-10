15bd0edb55f7d6564ea4a9d5117e4f4112878346
ojSeyi <1509941@rgu.ac.uk>
2016-08-10 11:48:56 +0100

    Commit

--- ajax/facilitycount.php
+++ ajax/facilitycount.php
@@ -1,7 +1,7 @@
 <?php
 $s = 0;
 if(isset($_POST['number'])){
-    $s = $s + $_POST['number'];
     $_SESSION['s'] = $s;
+    $_SESSION['s'] = $_SESSION['s'] + $_POST['number'];
     echo 'success';
 }

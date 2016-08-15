ecd38b3de6b84535ed1084c8bcef91691757d93a
ojSeyi <1509941@rgu.ac.uk>
2016-08-15 18:39:35 +0100

    Commit

--- facilitycount.php
+++ facilitycount.php
@@ -45,7 +45,7 @@ if(mysqli_num_rows($result) > 0) {
 
         }else{
             $_SESSION['newfacilityunavailable'] = 0;
-            if (count($facilityarray) <= $_SESSION['facilitysize']) {
+            if (count($facilityarray) <= 3) {
 
                 if (isset($facilityarray[0])) {
 

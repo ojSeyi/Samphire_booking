79363b1925ef2d3e8be49dfcaa7f626757ed35a8
ojSeyi <1509941@rgu.ac.uk>
2016-08-16 00:47:29 +0100

    Commit

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ if(isset($_SESSION['login'])){
 
     <div id="syscon"">
 
-        <form id="search" method="post" action="datecheck2.php" onload="mindate()">
+        <form id="search" method="post" action="datecheck2.php">
             <Label>Please select a facility</Label>
             <select name="facility" size="1" required>
                 <?php
@@ -53,7 +53,7 @@ if(isset($_SESSION['login'])){
                 ?>
             </select><br><br>
             <label>Reservation Date : </label>
-            <input id="startdate" name="startdate" type="date" disabled required/><br><br>
+            <input id="startdate" name="startdate" type="date" onload="mindate()" disabled required/><br><br>
             <label>If you would require the facility for more than one day tick this box</label><br>
             <input type="checkbox" id="enddate" name="enddateC" value="yes"/><br><br><br>
             <input type="submit" value="submit"/><br><br>

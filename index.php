0a48c9b27cb0a2510f56444e6e3b763c630ad37e
ojSeyi <1509941@rgu.ac.uk>
2016-08-16 01:57:07 +0100

    Commit

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ if(isset($_SESSION['login'])){
                 ?>
             </select><br><br>
             <label>Reservation Date : </label>
-            <input id="startdate" name="startdate" type="date" disabled required/><br><br>
+            <input id="startdate" name="startdate" type="date" onload="mindate()" disabled required/><br><br>
             <label>If you would require the facility for more than one day tick this box</label><br>
             <input type="checkbox" id="enddate" name="enddateC" value="yes"/><br><br><br>
             <input type="submit" value="submit"/><br><br>

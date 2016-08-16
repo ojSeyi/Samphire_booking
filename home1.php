7f78efe0fdae72d7c3a0e9080b50b3668cf276f3
ojSeyi <1509941@rgu.ac.uk>
2016-08-16 07:22:34 +0100

    Commit

--- home1.php
+++ home1.php
@@ -43,7 +43,7 @@ if(is_null($_SESSION['firstname'])){
             <Label>Please select a facility</Label>
             <select name="facility" size="1" required>
                 <?php
-                $getfacilities = "SELECT  FROM samphire_facilities";
+                $getfacilities = "SELECT * FROM samphire_facilities";
                 $result = mysqli_query($db, $getfacilities);
                 while ($row = mysqli_fetch_array($result))
                     echo "<option>". $row['f_name'] . "</option>" ;

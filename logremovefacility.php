cd46fa01f1ea072c11645febe352af29f2f294a1
ojSeyi <1509941@rgu.ac.uk>
2016-08-16 05:04:32 +0100

    Commit

---
+++ logremovefacility.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include ("db_connection.php"); ?>
+<?php
+session_start();
+if(is_null($_SESSION['firstname']) && is_null($_SESSION['facilityarraycheck'])){
+    header('location: bookstate.php');
+};
+?>
+<head>
+    <meta charset="UTF-8">
+    <title>Samphire-Subsea: Facility Reservation</title>
+    <link rel="stylesheet" href="assets/stylesheet.css">
+    <link rel="stylesheet" href="assets/unsemantic-grid-responsive-tablet.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <meta name="viewpoint"
+          content="width=device-width,
+          initial-scale=1,
+          minimum-scale=1,
+          maximum-scale=1"/>
+</head>
+<head>
+    <meta charset="UTF-8">
+    <title>Samphire Subsea Facilities Booking</title>
+</head>
+<body>
+<header>
+    <img src="assets/images/logo_2016.jpg" id="logo"/>
+    <div id="logout">
+        <form method="post" action="logout.php">
+            <label><?php echo $_SESSION['firstname'];?></label>
+            <input type="submit" name="logout" value="logout" id="logoutbutton"/>
+        </form>
+    </div>
+    <div id="pagetitle"><h4>Samphire-Subsea</h4><p>Facilities Booking System</p></div>
+</header>
+<main>
+    <section id="bannerbox">
+        <img src="assets/images/banner1.jpg" id="bannerimage"/>
+    </section>
+
+    <div id="syscon">
+
+    </div>
+</main>
+</body>
+</html>
---
+++ removefacility.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include ("db_connection.php"); ?>
+<?php
+session_start();
+if(is_null($_SESSION['facilityarraycheck'])){
+    header('location: bookstate.php');
+};
+?>
+<head>
+    <meta charset="UTF-8">
+    <title>Samphire-Subsea: Facility Reservation</title>
+    <link rel="stylesheet" href="assets/stylesheet.css">
+    <link rel="stylesheet" href="assets/unsemantic-grid-responsive-tablet.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <meta name="viewpoint"
+          content="width=device-width,
+          initial-scale=1,
+          minimum-scale=1,
+          maximum-scale=1"/>
+</head>
+<head>
+    <meta charset="UTF-8">
+    <title>Samphire Subsea Facilities Booking</title>
+</head>
+<body>
+<header>
+    <img src="assets/images/logo_2016.jpg" id="logo"/>
+    <div id="form">
+        <form method="post" action="login.php">
+            <input type="text" name="username" id="usernamebox" placeholder="Username" required/>
+            <input type="password" name="password" id="passwordbox" placeholder="Password" required/>
+            <input type="submit" value="Login" name="login" id="loginb"/>
+        </form>
+    </div>
+    <div id="pagetitle"><h4>Samphire-Subsea</h4><p>Facilities Booking System</p></div>
+</header>
+<main>
+    <section id="bannerbox">
+        <img src="assets/images/banner1.jpg" id="bannerimage"/>
+    </section>
+
+    <div id="syscon">
+
+    </div>
+</main>
+</body>
+</html>

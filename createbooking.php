1c3027a8a0930310fd36b3ce41708c03e0a345a6
ojSeyi <1509941@rgu.ac.uk>
2016-08-16 06:45:42 +0100

    Commit

--- createbooking.php
+++ createbooking.php
@@ -11,7 +11,15 @@ session_start();
             return $randomString;
     }
 
-        $bookingconfirmationnumber = generateRandomString();
+    $bookingconfirmationnumber = generateRandomString();
+
+    $availables = "SELECT * FROM guest_bookings WHERE reference = '$bookingconfirmationnumber'";
+    $resultsavailables = mysqli_query($db, $availables) or die("failed");
+    if(mysqli_num_rows($resultsavailables) > 0){
+        header('location: createbooking.php');
+    }else{
+
+    }
 
     $facilities = $_SESSION['facilityarray'];
     $startdate = date("Y-m-d",strtotime($_SESSION['start']));
@@ -20,6 +28,57 @@ session_start();
     }else{
         $enddate = $startdate;
     }
+
+    $userfullname = $_SESSION['userfullname'];
+    $custid = $userfullname['cust_id'];
+
+    $totalcost = 0;
+    foreach ($facilities as $savecost) {
+        $checkcost = $savecostcost;
+        $getfacilities = "SELECT * FROM samphire_facilities WHERE f_name = '$checkcost'";
+        $result = mysqli_query($db, $getfacilities);
+        $cost = mysqli_fetch_array($result);
+        $totalcost = $totalcost + $cost['cost'];
+    }
+    $cost = $totalcost;
+
+
+foreach($facilities as $facility) {
+    $fac = "SELECT * FROM samphire_facilities WHERE f_name = '$newfacility'";
+    $result = mysqli_query($db, $fac);
+    $rows = 0;
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_array($result);
+        $rows = $row['f_id'];
+    }else{
+        header('location: index1.php');
+    }
+    $insertrecord = "Insert Into customer_bookings (reference, f_id, cust_id, startdate, enddate, price) VALUES ('$bookingconfirmationnumber', '$rows', '$custid', '$startdate', '$enddate', '$cost')";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

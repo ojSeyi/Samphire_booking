5d67f48665ac35f3b996d81dec5ae49b2676a218
ojSeyi <1509941@rgu.ac.uk>
2016-08-17 04:35:06 +0100

    Commit

--- bookstate.php
+++ bookstate.php
@@ -109,27 +109,27 @@ if(is_null($_SESSION['facili']) && is_null($_SESSION['start'])){
         </div>
 
 
-        <div><label>To add another facility, select facility and click 'add':  </label></div>
+        <div><label>To add another facility, select facility and click 'add':  </label></div><br><br><br><br>
+        <div id="warning"><label>You cannot select the same facility</label></div>
         <div>
-        <form method="post" action="facilitycount.php">
-            <input type="hidden" name="bounce" value="bounce">
-            <input type="hidden" name="firstfacility" id="firstfacility" value="<?php $_SESSION['facili'] ?>">
-            <select name="facilityarray" id="facilityarray" size="1" required>
-                <?php
-                $getfacilities = "SELECT * FROM samphire_facilities";
-                $result = mysqli_query($db, $getfacilities);
-                while ($row = mysqli_fetch_array($result)){
-                    if($row['f_name'] ==  $facilities[0]) {
+            <form method="post" action="facilitycount.php">
+                <select name="facilityarray" id="facilityarray" size="1" required>
+                    <?php
+                    $getfacilities = "SELECT * FROM samphire_facilities";
+                    $result = mysqli_query($db, $getfacilities);
+                    while ($row = mysqli_fetch_array($result)){
+                        if($row['f_name'] ==  $facilities[0]) {
 
-                    }else{
-                        echo "<option>" . $row['f_name'] . "</option>";
+                        }else{
+                            echo "<option>" . $row['f_name'] . "</option>";
+                        }
                     }
-                }
-                ?>
-            </select><br><br>
-            <input type='submit' name="addfacility" id="addfacility" value='Add Facility'>
-        </form><br><br>
-        </div>
+                    ?>
+                </select><br><br>
+                <input type="hidden" name="firstfacility" id="firstfacility" value="<?php $_SESSION['facili'] ?>">
+                <input type='submit' name="addfacility" id="addfacility" value='Add Facility'>
+            </form>
+        </div><br><br><br>
 
         <div id="submitbooking">
             <form method="post" action="jsfggksvuaigfuakwjygviuaevfvdkuvjy.php">

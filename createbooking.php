fbdff32a075fa41943dcbd3dd61cec01adbc5c16
ojSeyi <1509941@rgu.ac.uk>
2016-08-17 20:46:42 +0100

    Commit

--- createbooking.php
+++ createbooking.php
@@ -118,56 +118,8 @@ function displayfacilities(){
         return $showfacility ."<br>";
     }
 }
-//email body in html
-//ATTENTION, THE LINK MAY POINT TO THE MASTER DOMAIN, RATHER THAN YOUR OWN xxx.PHP
-$txt = "Dear $firstname,
-					<br><br>
-					Your booking has been created.
-					<br>
-					Here is your reference number: <h2> $bookingconfirmationnumber </h2>
-					<br>
-					Here are the details of your booking
-					<br>
-					<table>
-					    <tr>
-					        <th>Facility</th>
-					        <th>Price</th><br>
-                        </tr>
-                        <tr>
-                            <td>".
-                                displayfacilities()
-                            ."</td>
-                            <td>".
-                                displayprices()
-                            ."</td>
-                        </tr>
-                        <tr>
-                            <td>Total: </td>
-                            <td>". $totalcost . "</td>
-                        </tr>
-                    </table>
 
-					<br><br>
-					Thank you for choosing Samphire-Subsea facilities!
-					<br>
-					Please contact the facilities department on nomail@samphire-subsea.com with any complaints or enquiries.
-					<br><br>
-					King Regards,
-					<br><br>
-					Samphire Subsea Facilities";
-
-//take in the necessary swiftmailer code
-require_once 'swiftmailer/lib/swift_required.php';
-
-$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465,'ssl')->setUsername('ojtestall@gmail.com')->setPassword('Oluwas3yi');
-
-$mailer = Swift_Mailer::newInstance($transport);
-$message = Swift_Message::newInstance('Samphire Subsea Facilities: Reservation')
-    ->setFrom(array('ojtestall@gmail.com' => 'Samphire Facility Reservation'))
-    ->setTo(array($h_email => $h_email))
-    ->setBody($txt, 'text/html');
-$mailer->send($message);
-
+echo $_SESSION['custemail'];
 
 
 

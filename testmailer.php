32f48cf965bfa30414679479fe168adc47ba73a8
ojSeyi <1509941@rgu.ac.uk>
2016-08-18 00:56:19 +0100

    Commit

--- testmailer.php
+++ testmailer.php
@@ -1,26 +1,32 @@
 <?php
 
-require_once 'swiftmailer/lib/swift_init.php';
-
-$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
-    ->setUsername('oluwaseyiny@gmail.com')
-    ->setPassword('Oluwas3y1.')
-;
+//email subject
+$subject = "Friendship Link has been created for you!";
+//email body in html
+//ATTENTION, THE LINK MAY POINT TO THE MASTER DOMAIN, RATHER THAN YOUR OWN xxx.PHP
+$txt = "Dear $name,
+<br><br>
+Thank you for being a part of International Students Friendship Link
+<br>
+You have a match on the friendship link program.
+<br>
+This match was made based on your preferences at sign-up:
+<br>
+Gender prefrence: $gender
+Interested Nationality to host: $nation
+Hobbies: $hobbies
+<br><br>
+Please contact Kathrine on kathrine@friendsUk.com for more infomation on the student selected.
+<br><br>
+King Regards,
+<br><br>
+The Friendship link";
+//take in the necessary swiftmailer code
+require_once 'swiftmailer/lib/swift_required.php';
+$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465,'ssl')->setUsername('ukpehmfon@gmail.com')->setPassword('seveneleven711');
 $mailer = Swift_Mailer::newInstance($transport);
-
-$message = Swift_Message::newInstance();
-$message->setSubject('My subject');
-$message -> setBody('FUck all yall');
-$message->setTo('seyiyusuf100@gmail.com');
-$message->setFrom('oluwaseyiny@gmail.com');
-
+$message = Swift_Message::newInstance('FriendshipLink: student matched')
+->setFrom(array('ukpehmfon@gmail.com' => 'Friendship link'))
+->setTo(array($h_email => $h_email))
+->setBody($txt, 'text/html');
 $mailer->send($message);
-
-if ($mailer->send($message))
-{
-    echo "Sent\n";
-}
-else
-{
-    echo "Failed\n";
-}

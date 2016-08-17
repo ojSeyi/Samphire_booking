584bf2fdab7fdc3326499276b9e94e39485e2081
ojSeyi <1509941@rgu.ac.uk>
2016-08-18 00:58:26 +0100

    Commit

--- testmailer.php
+++ testmailer.php
@@ -26,7 +26,7 @@ require_once 'swiftmailer/lib/swift_required.php';
 $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465,'ssl')->setUsername('ukpehmfon@gmail.com')->setPassword('seveneleven711');
 $mailer = Swift_Mailer::newInstance($transport);
 $message = Swift_Message::newInstance('FriendshipLink: student matched')
-->setFrom(array('ukpehmfon@gmail.com' => 'Friendship link'))
-->setTo(array($h_email => $h_email))
+->setFrom(array('oluwaseyiny@gmail.com' => 'Friendship link'))
+->setTo(array('oluwaseyiny@gmail.com' => 'oluwaseyiny@gmail.com'))
 ->setBody($txt, 'text/html');
 $mailer->send($message);

4c3ba91f688dd3ba6293b5c83c578530b5a6961d
ojSeyi <1509941@rgu.ac.uk>
2016-08-18 14:04:56 +0100

    Commit

--- confirmation.php
+++ confirmation.php
@@ -1,7 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include ("db_connection.php"); ?>
 <?php
-
-
-
-
-
+session_start();
+if(is_null($_SESSION['firstname']) && is_null($_SESSION['facilityarraycheck'])){
+    header('location: bookstate.php');
+};
 ?>
+<head>
+    <meta charset="UTF-8">
+    <title>Samphire-Subsea: Facility Reservation</title>
+    <link rel="stylesheet" href="assets/stylesheet.css">
+    <link rel="stylesheet" href="assets/unsemantic-grid-responsive-tablet.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <meta name="viewpoint"
+          content="width=device-width,
+          initial-scale=1,
+          minimum-scale=1,
+          maximum-scale=1"/>
+</head>
+<head>
+    <meta charset="UTF-8">
+    <title>Samphire Subsea Facilities Booking</title>
+</head>
+<body>
+<header class="grid-container">
+    <img src="assets/images/logo_2016.jpg" id="logo"/>
+    <div id="log">
+        <div id="logout">
+            <form method="post" action="logout.php">
+                <label><?php echo $_SESSION['firstname'];?></label>
+                <input type="submit" name="logout" value="logout" id="logoutbutton"/>
+            </form>
+        </div>
+        <div id="pagetitle"><h4>Samphire-Subsea</h4><p>Facilities Booking System</p></div>
+    </div>
+    <nav id="upnav" class="grid-container">
+        <ul>
+            <li><a href='index.php'>Create Booking</a></li>
+            <li><a href='showbugs.php?bugcategory=android'>Manage Booking</a></li>
+            <li><a href='showbugs.php?bugcategory=ios'>Contact Us</a></li>
+        </ul>
+    </nav>
+</header>
+
+<div id="system">
+    <main class="grid-container">
+
+        <div id="syscon">
+
+        </div>
+
+    </main>
+</div>
+
+</body>
+</html>

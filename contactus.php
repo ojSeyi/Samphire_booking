670d81baf21e98f621ac9b117c6da60f696ad266
ojSeyi <1509941@rgu.ac.uk>
2016-08-18 20:46:42 +0100

    Commit

--- contactus.php
+++ contactus.php
@@ -1,7 +1,53 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: OJ Pumping
- * Date: 18/08/2016
- * Time: 20:39
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Samphire-Subsea: Facility Reservation</title>
+    <link rel="stylesheet" href="assets/stylesheet.css">
+    <link rel="stylesheet" href="assets/unsemantic-grid-responsive-tablet.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.0/themes/base/jquery-ui.css">
+    <meta name="viewpoint"
+          content="width=device-width,
+          initial-scale=1,
+          minimum-scale=1,
+          maximum-scale=1"/>
+</head>
+
+<body>
+
+<header class="grid-container">
+    <img src="assets/images/logo_2016.jpg" id="logo"/>
+    <div id="log">
+        <div id="form">
+            <form method="post" action="login.php">
+                <input type="text" name="username" id="usernamebox" placeholder="Username" required/>
+                <input type="password" name="password" id="passwordbox" placeholder="Password" required/>
+                <input type="submit" value="Login" name="login" id="loginb"/>
+            </form>
+        </div>
+        <div id="pagetitle"><h4>Samphire-Subsea</h4><p>Facilities Booking System</p></div>
+    </div>
+    <nav id="upnav" class="grid-container">
+        <ul>
+            <li><a href='index.php'>Create Booking</a></li>
+            <li><a href='managebooking.php'>Manage Booking</a></li>
+            <li><a href='contactus.php'>Contact Us</a></li>
+            <li><a href='registration.php'>Register</a></li>
+        </ul>
+    </nav>
+</header>
+
+
+<div id="system">
+    <main class="grid-container">
+
+        <div id="syscon">
+
+        </div>
+
+    </main>
+</div>
+
+</body>
+</html>

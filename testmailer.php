69cb6f750d0b6078715fbc09745ecbdb8f6ab889
ojSeyi <1509941@rgu.ac.uk>
2016-08-18 02:23:58 +0100

    Commit

--- testmailer.php
+++ testmailer.php
@@ -41,9 +41,9 @@ $txt = "Dear $firstname,
 
 try{
 
-$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465,'ssl')
-->setUsername('ojtestall@gmail.com')
-->setPassword('Oluwas3yi');
+$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 587,'tls')
+    ->setUsername('ojtestall@gmail.com')
+    ->setPassword('Oluwas3yi');
 
 $mailer = Swift_Mailer::newInstance($transport);
 $message = Swift_Message::newInstance('Samphire')

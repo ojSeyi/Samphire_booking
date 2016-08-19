73292e3a08d768d0a9acee5dd0dcbe041c649b83
ojSeyi <1509941@rgu.ac.uk>
2016-08-19 05:26:21 +0100

    Commit

--- bookinglocator.php
+++ bookinglocator.php
@@ -47,56 +47,68 @@ echo "Enter Reference number and lastname";
 include ("db_connection.php");
 
 
-    $lastname = $_POST['lastname'];
-    $confirmation = $_POST['confirmation'];
-echo $lastname;
-    $lastname = stripcslashes($lastname);
-    $confirmation = stripcslashes($confirmation);
-    $lastname = mysqli_real_escape_string($db, $lastname);
-    $confirmation = mysqli_real_escape_string($db,$confirmation);
-    $firstname = "";
-    $custid = "";
+$lastname = $_POST['lastname'];
+$confirmation = $_POST['confirmation'];
+$lastname = stripcslashes($lastname);
+$confirmation = stripcslashes($confirmation);
+$lastname = mysqli_real_escape_string($db, $lastname);
+$confirmation = mysqli_real_escape_string($db,$confirmation);
 
 
-    $getfacilitycommand = "SELECT f_id FROM samphire_facilities";
-    $fetchfacilities = mysqli_query($db, $getfacilitycommand);
-    $f_idarray = Array();
-    if(!$fetchfacilities){
-        echo "nigga i aint working";
-    }
-    while ($row = mysqli_fetch_array($fetchfacilities, MYSQLI_NUM)) {
-        $f_idarray[] =  $row['f_id'];
-    }
-    foreach($f_idarray as $p){
-        echo $p;
-    }
+$getfacilitycommand = "SELECT f_id FROM samphire_facilities";
+$fetchfacilities = mysqli_query($db, $getfacilitycommand);
+$f_idarray = Array();
+if(!$fetchfacilities){
+    echo "nigga i aint working";
+}
+while ($row = mysqli_fetch_array($fetchfacilities, MYSQLI_ASSOC)) {
+    $f_idarray[] =  $row['f_id'];
+}
 
-    $facilities = array();
-    foreach($f_idarray as $facili){
-        $bookingcommand = "SELECT * FROM customer_bookings WHERE f_id = '$facili' AND reference = '$confirmation'";
-        $fetchbookings = mysqli_query($db, $bookingcommand);
-        if(mysqli_num_rows($fetchbookings) > 0){
-                $facilities[] = $facili;
-        }else{
 
+$facilities = array();
+foreach($f_idarray as $facili){
+    $bookingcommand = "SELECT * FROM customer_bookings WHERE f_id = '$facili' AND reference = '$confirmation'";
+    $fetchbookings = mysqli_query($db, $bookingcommand);
+    if(mysqli_num_rows($fetchbookings) > 0){
+        while ($ross = mysqli_fetch_array($fetchbookings, MYSQLI_ASSOC)) {
+            $facilities[] =  $ross['f_id'];
         }
+    }else{
 
     }
-    $getdatescommand = "SELECT startdate, enddate FROM customer_bookings WHERE f_id = '$facilities[0]' AND reference = '$confirmation'";
-    $fetchdates = mysqli_query($db, $getdatescommand);
-    $dates = mysqli_fetch_array($fetchdates);
-    $startdate = $dates['startdate'];
-    $enddate = $dates['enddate'];
+}
 
-    $facilitycosts = array();
-    foreach ($facilities as $showcost) {
-        $checkcost = $showcost;
-        $getfacilities = "SELECT * FROM samphire_facilities WHERE f_name = '$checkcost'";
-        $result = mysqli_query($db, $getfacilities);
-        $cost = mysqli_fetch_array($result);
-        $costs = $cost['cost'];
-        $facilitycosts[] = $costs;
-    }
+
+$getdatescommand = "SELECT startdate, enddate FROM customer_bookings WHERE f_id = '$facilities[0]' AND reference = '$confirmation'";
+$fetchdates = mysqli_query($db, $getdatescommand);
+$dates = mysqli_fetch_array($fetchdates);
+$startdate = $dates['startdate'];
+$enddate = $dates['enddate'];
+
+$getidcommand = "SELECT cust_id FROM customer_bookings WHERE f_id = '$facilities[0]' AND reference = '$confirmation'";
+$fetchid = mysqli_query($db, $getidcommand);
+$id = mysqli_fetch_array($fetchdates);
+$custid = $id['cust_id'];
+
+$getfirstname = "SELECT firstname FROM customers WHERE cust_id = '$custid'";
+$fetchfirstname = mysqli_query($db, $getfirstname);
+$first = mysqli_fetch_array($fetchdates);
+$firstname = $dates['firstname'];
+
+$facilitycosts = array();
+$facilityname = array();
+foreach ($facilities as $showcost) {
+    $checkcost = $showcost;
+    $getcosts = "SELECT * FROM samphire_facilities WHERE f_id = '$checkcost'";
+    $result = mysqli_query($db, $getcosts);
+    $cost = mysqli_fetch_array($result);
+    $costs = $cost['cost'];
+    $name = $cost['f_name'];
+    $facilitycosts[] = $costs;
+    $facilityname[] = $name;
+}
+
 
 echo $lastname ;
 echo $confirmation;

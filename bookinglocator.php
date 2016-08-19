f68f485f709d65207e524087726271ffea33d5ca
ojSeyi <1509941@rgu.ac.uk>
2016-08-19 02:53:19 +0100

    Commit

--- bookinglocator.php
+++ bookinglocator.php
@@ -46,7 +46,10 @@ if(empty($_POST['confirmation']) || empty($_POST['lastname'])) {
 }else {$proceed = 1;}
     $lastname = $_POST['lastname'];
     $confirmation = $_POST['confirmation'];
-
+    $lastname = stripcslashes($lastname);
+    $confirmation = stripcslashes($confirmation);
+    $lastname = mysqli_real_escape_string($db, $lastname);
+    $confirmation = mysqli_real_escape_string($db,$confirmation);
     $firstname = "";
     $custid = "";
     $customer = "";

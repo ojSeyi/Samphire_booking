1731a330d5618aff27f7b99c506b888a79684198
ojSeyi <1509941@rgu.ac.uk>
2016-08-20 16:36:07 +0100

    Commit

--- editbooking.php
+++ editbooking.php
@@ -1,5 +1,5 @@
 <?php
-if(is_null($_POST['flow']) && is_null($_POST['lastname'])) {
+if(is_null($_POST['flow']) || is_null($_SESSION['facilitynam'])) {
     header('location: locatebooking.php');
 }else {$proceed = 1;}
 ?>

647988c7651da4896574e97b094d2bfb26abcead
ojSeyi <1509941@rgu.ac.uk>
2016-08-21 21:53:50 +0100

    Commit

--- backend/changebookings.php
+++ backend/changebookings.php
@@ -1,7 +1,75 @@
 <?php
 session_start();
+include ('db_connection.php');
 if(is_null($_SESSION['admin'])){
     header('location: adminlogin.php');
+}
+$run = 0;
+$firstname = "";
+$lastname = "";
+$startdate = "";
+$enddate = "";
+$price = "";
+$bookedfacilities = array();
+$bookedfacilitiescost = array();
+if(isset($_POST['confirmation'])){
+    $reference = $_POST['confirmation'];
+    $reference = stripcslashes($reference);
+    $reference = mysqli_real_escape_string($db, $reference);
+    $query = "SELECT * FROM customer_bookings WHERE reference = '$reference'";
+    $run = mysqli_query($db, $query);
+    if(mysqli_num_rows($run) < 1){
+        $msg = 'No result';
+    }else{
+        $k = 1;
+        while($o = mysqli_fetch_array($run)){
+            $custid = $o['cust_id'];
+            $query2 = "SELECT * FROM customers WHERE cust_id = '$custid'";
+            $run2 = mysqli_query($db, $query2);
+            $fetch = mysqli_fetch_array($run2);
+            $firstname = $fetch['firstname'];
+            $lastname = $fetch['lastname'];
+            $startdate = $o['startdate'];
+            $enddate = $o['enddate'];
+            $price = $o['price'];
+
+            $fid =$o['f_id'];
+            $query3 = "SELECT * FROM samphire_facilities WHERE f_id = '$fid'";
+            $run3 = mysqli_query($db, $query3);
+            $fetch2 = mysqli_fetch_array($run3);
+            $bookedfacilities[] = $fetch2['f_name'];
+            $bookedfacilitiescost[] = $fetch2['cost'];
+        }
+        $_SESSION['confirmation'] = $reference;
+    }
+
+}else{
+    $reference = $_SESSION['confirmation'];
+    $query = "SELECT * FROM customer_bookings WHERE reference = '$reference'";
+    $run = mysqli_query($db, $query);
+    if(mysqli_num_rows($run) < 1){
+        $msg = 'No result';
+    }else{
+        $k = 1;
+        while($o = mysqli_fetch_array($run)){
+            $custid = $o['cust_id'];
+            $query2 = "SELECT * FROM customers WHERE cust_id = '$custid'";
+            $run2 = mysqli_query($db, $query2);
+            $fetch = mysqli_fetch_array($run2);
+            $firstname = $fetch['firstname'];
+            $lastname = $fetch['lastname'];
+            $startdate = $row['startdate'];
+            $enddate = $row['enddate'];
+            $price = $row['price'];
+
+            $fid =$o['f_id'];
+            $query3 = "SELECT * FROM samphire_facilities WHERE f_id = '$fid'";
+            $run3 = mysqli_query($db, $query3);
+            $fetch2 = mysqli_fetch_array($run3);
+            $bookedfacilities[] = $fetch2['f_name'];
+            $bookedfacilitiescost[] = $fetch2['cost'];
+        }
+    }
 }
 
 
@@ -44,11 +112,76 @@ if(is_null($_SESSION['admin'])){
         </ul>
     </nav>
     <section class="grid-70">
-        <div id="system" class="grid-container">
+        <div id="system">
             <div id="screen" class="grid-container">
-                <div id="screen" class="grid-container">
-
-                </div>
+                <table id="bookingdetails" class="grid-container">
+                    <tr>
+                        <td id="o">Customer Name: </td>
+                        <td id="o"><?php echo $firstname . " " . $lastname; ?></td>
+                    </tr>
+                    <tr id="ref">
+                        <td>Booking Reference Number:</td>
+                        <td><?php echo $reference ?></td>
+                    </tr>
+                    <tr>
+                        <td>Booking Date(s): </td>
+                        <?php
+                        if($enddate == $startdate){
+                            echo "<td>".$startdate."</td>";
+                        }else{
+                            echo "<td> From: ".$startdate." to: " . $enddate . "</td>";
+                        }
+                        ?>
+                    </tr>
+                    <tr id="pins">
+                        <td>Facility(s)</td>
+                        <td>Price(s)</td>
+                    </tr>
+                    <tr id="pin">
+                        <td>
+                            <?php
+                            foreach ($bookedfacilities as $showfacility) {
+                                echo $showfacility ."<br>";
+                            }
+                            ?>
+                        </td>
+                        <td>  <?php
+                            foreach ($bookedfacilities as $showcost) {
+                                $checkcost = $showcost;
+                                $getfacilities = "SELECT * FROM samphire_facilities WHERE f_name = '$checkcost'";
+                                $result = mysqli_query($db, $getfacilities);
+                                $cost = mysqli_fetch_array($result);
+                                echo "£".$cost['cost'] . "<br>";
+                            }
+                            ?>
+                        </td>
+                    </tr><br><br>
+                    <tr id="pind">
+                        <td>Total: </td>
+                        <td><?php
+                            $totalcost = 0;
+                            foreach ($bookedfacilities as $showcost) {
+                                $getfacilities = "SELECT * FROM samphire_facilities WHERE f_name = '$showcost'";
+                                $result = mysqli_query($db, $getfacilities);
+                                $cost = mysqli_fetch_array($result);
+                                $totalcost = $totalcost + $cost['cost'];
+                            }
+                            echo "£".$totalcost;
+                            ?>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div id="search">
+                <table>
+                    <form method="post" action="viewbookings.php">
+                        <tr>
+                            <td><label>Enter reference number: </label></td>
+                            <td><input type="text" name="confirmation" required><br></td>
+                            <td><input type="submit" value="search"><br></td>
+                        </tr>
+                    </form>
+                </table>
             </div>
         </div>
     </section>

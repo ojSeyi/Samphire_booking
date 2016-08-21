38fc45b094db2436a95e94d4616764a016449554
ojSeyi <1509941@rgu.ac.uk>
2016-08-21 21:07:11 +0100

    Commit

--- backend/viewcustomers.php
+++ backend/viewcustomers.php
@@ -204,7 +204,9 @@ if(isset($_POST['username'])){
                     </table><br><br>";
                     }elseif($k == 2){
                         echo "
-                            <div id='customers'><table><tr><th>Firstname</th><th>Lastname</th><th>Username</th><th>Password</th><th>Email</th><th>Mobile</th><th>Address</th></tr>";
+                            <div id='customers'><table>
+                            <caption>Here's a list of customers with the name". $firstname." and ".$lastname."</caption>
+                            <tr><th>Firstname</th><th>Lastname</th><th>Username</th><th>Password</th><th>Email</th><th>Mobile</th><th>Address</th></tr>";
                             $i = 0;
                             foreach($firstnamearray as $firstname){
                             echo "<tr>"."<td>". $firstname ."<td>". $lastnamearray[$i] ."<td>". $usernamearray[$i] ."<td>". $passwordarray[$i] ."<td>". $emailsarray[$i] ."<td>". $mobilearray[$i] ."<td>". $addressarray[$i]."<tr>";

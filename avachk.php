c8d5b205c567bff636e5fda174d72b623d73d1f3
ojSeyi <1509941@rgu.ac.uk>
2016-08-23 20:13:13 +0100

    Commit

--- avachk.php
+++ avachk.php
@@ -122,30 +122,36 @@ if(is_null($_SESSION['startdates'])){
                     if(count($datesinrange) > 31){
                         echo "<label>For booking greater than 30 days please contact the office using the information in the contact page</label>";
                     }else{
-                    $unavailabledates = array();
+                        $k = 0;
+                        $unavailabledates = array();
                         foreach($datesinrange as $date){
                             $availablerange = "SELECT * FROM customer_bookings WHERE f_id = '$rows' AND (startdate <= '$date' AND enddate >= '$date')";
                             $results = mysqli_query($db, $availablerange) or die("failed");
-                            if(mysqli_num_rows($results) > 0){
+                            if(mysqli_num_rows($results) > 0) {
                                 $unavailabledates[] = $date;
-                                echo "<div id='screen'><div id='filled dates'><div id='bookingdetail'>          The <h4>$facility</h4> is unavailable on the following dates: </label><br>";
-                                echo "<table><tr>";
-                                foreach($unavailabledates as $showdate){
-                                    echo "<td>".$showdate."</td>";
-                                }
-                                echo "</tr></table></div></div></div>";
-                                echo "<form id='search' method='post' action='datecheck.php'>
+                                $k++;
+                            }else{
+
+                            }
+                        }
+                        if($k > 0){
+                            echo "<div id='screen'><div id='filled dates'><div id='bookingdetail'>          The <h4>$facility</h4> is unavailable on the following dates: <br>";
+                            echo "<table><tr>";
+                            foreach($unavailabledates as $showdate){
+                                echo "<td>".$showdate."</td>";
+                            }
+                            echo "</tr></table></div></div></div>";
+                            echo "<form id='search' method='post' action='logdatecheck.php'>
                                     <label>Please select different dates: </label><br><br>
-                                    <input id='startdate' name='startdate' type='date' value='$currentdate'/><br><br>
-                                    <input id='enddate' name='enddate' type='date' value='$currentnextdate'/><br><br>
+                                    <input id='startdate' name='startdate' type='text' value='Click here to select a date'/><br><br>
+                                    <input id='enddate' name='enddate' type='text' value='Click here to select a date'/><br><br>
                                     <input type='submit' value='Check' />
                                     </form>";
-                            }else{
-                                $_SESSION['Start'] = $startdates;
-                                $_SESSION['end'] = $enddates;
-                                $_SESSION['facili'] = $facility;
-                                header('location: bookstate.php');
-                            }
+                        }else{
+                            $_SESSION['Start'] = $startdates;
+                            $_SESSION['end'] = $enddates;
+                            $_SESSION['facili'] = $facility;
+                            header('location: logbookstate.php');
                         }
                     }
 

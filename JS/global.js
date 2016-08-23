fd022f7ce3bac70c659c57bcfb906b15055b303c
ojSeyi <1509941@rgu.ac.uk>
2016-08-23 18:17:54 +0100

    Commit

--- JS/global.js
+++ JS/global.js
@@ -1,3 +1,4 @@
 $('#startdate').datepicker({dateformt: 'dd/mm/yy', minDate: 0 });
 $('#enddate').datepicker({dateformt: 'dd/mm/yy', minDate: 0 });
 
+$('#enddatec').onclick(document.getElementById('enddate').setAttribute('type', 'date'));

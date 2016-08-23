6112708b266c132779e30fb83f39263996d6ec4b
ojSeyi <1509941@rgu.ac.uk>
2016-08-23 18:56:17 +0100

    Commit

--- JS/global.js
+++ JS/global.js
@@ -3,9 +3,11 @@ $('#enddate').datepicker({dateformt: 'dd/mm/yy', minDate: 0 });
 
 var checkbox = document.getElementById('enddatec');
 var delivery_div = document.getElementById('showend');
+var enddate = document.getElementById('enddate');
 var showHiddenDiv = function(){
     if(checkbox.checked) {
         delivery_div.style['display'] = 'block';
+        enddate.getAttribute('required', true);
     } else {
         delivery_div.style['display'] = 'none';
     }

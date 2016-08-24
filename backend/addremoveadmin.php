39289a655c043d0a48aaea8917dc6e9ed0cc8adf
ojSeyi <1509941@rgu.ac.uk>
2016-08-24 01:12:28 +0100

    Commit

---
+++ backend/addremoveadmin.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+include ('db_connection.php');
+if(is_null($_SESSION['admin'])){
+    header('location: adminlogin.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Samphire-Subsea: Facility Reservation</title>
+    <link rel="stylesheet" href="assets/stylesheet.css">
+    <link rel="stylesheet" href="assets/unsemantic-grid-responsive-tablet.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.0/themes/base/jquery-ui.css">
+    <meta name="viewpoint"
+          content="width=device-width,
+          initial-scale=1,
+          minimum-scale=1,
+          maximum-scale=1"/>
+
+</head>
+<body>
+<header>
+    <img src="assets/images/logo_2016.jpg" id="logo"/>
+    <div id="log">
+        <div id="logout">
+            <form method="post" action="adminlogout.php">
+                <label><?php echo $_SESSION['firstname'];?></label>
+                <input type="submit" name="logout" value="logout" id="logoutbutton"/>
+            </form>
+        </div>
+        <div id="pagetitle"><h4>Samphire-Subsea</h4><p>Facilities Booking System - Administrator</p></div>
+</header>
+
+<main>
+    <nav class="grid-30">
+        <ul>
+            <li><a href='viewbookings.php'>View Bookings</a></li>
+            <li><a href='changebookings.php'>Change Bookings</a></li>
+            <li><a href='cancelbookings.php'>Cancel Bookings</a></li>
+            <li><a href='refreshsystem.php'>Refresh System</a></li>
+            <li><a href='viewcustomers.php'>View Customers</a></li>
+        </ul>
+    </nav>
+    <section class="grid-70">
+        <div id="system" class="grid-container">
+            <div id="screen" class="grid-container">
+
+            </div>
+        </div>
+    </section>
+</main>
+
+</body>
+</html>

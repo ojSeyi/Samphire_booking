f83b6a33f721aa87907413c586c7abe32d076a53
ojSeyi <1509941@rgu.ac.uk>
2016-08-24 01:59:50 +0100

    Commit

--- backend/JS/global.js
+++ backend/JS/global.js
@@ -1,18 +1,21 @@
 $('#startdate').datepicker({dateformt: 'dd/mm/yy', minDate: 0 });
 $('#enddate').datepicker({dateformt: 'dd/mm/yy', minDate: 0 });
 
-function open(){
+
     var add = document.getElementById('ad');
     var del = document.getElementById('de');
     var div1 = document.getElementById('search01');
     var div2 = document.getElementById('search02');
 
-    if(add.click()) {
-        div1.style['display'] = 'block';
-        div2.style['display'] = 'none';
-    }else if(del.click()){
-        div2.style['display'] = 'block';
-        div1.style['display'] = 'none';
-    }
+$('#ad').click(function(){
+    div1.style['display'] = 'block';
+    div2.style['display'] = 'none';
+});
 
-}
+$('#de').click(function(){
+    div2.style['display'] = 'block';
+    div1.style['display'] = 'none';
+});
+
+
+

6718bca902b9bb70e7498bc5bb57eb005204c299
ojSeyi <1509941@rgu.ac.uk>
2016-08-24 12:40:57 +0100

    Commit

--- backend/changebookings.php
+++ backend/changebookings.php
@@ -131,7 +131,7 @@ if(isset($_POST['rfacility'])){
         echo $cus;
         $newt = $priceee + $ku;
         echo $newt;
-        $addcmd = "INSERT INTO customer_bookings (reference, f_id, cust_id, startdate, enddate, price) VALUES ('$reference', '$x', '$cus', '$startdates', $enddates, '$priceee')";
+        $addcmd = "INSERT INTO customer_bookings (reference, f_id, cust_id, startdate, enddate, price) VALUES ('$reference', '$x', '$cus', '$startdates', '$enddates', '$priceee')";
         $run = mysqli_query($db, $addcmd)or die('lol5');
         $sqll = "UPDATE customer_bookings SET price='$newt' WHERE reference= $reference";
         $runsql = mysqli_query($db, $sqll) or die('lol6');

9f688a84bbf8579ab20eaf7d97a8420bf0d166ab
ojSeyi <1509941@rgu.ac.uk>
2017-01-05 16:29:36 +0100

    Commit

--- test.php
+++ test.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Samphire-Subsea: Facility Reservation</title>
+    <link rel="stylesheet" href="assets/stylesheet.css">
+    <link rel="stylesheet" href="assets/unsemantic-grid-responsive-tablet.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <meta name="viewpoint"
+          content="width=device-width,
+          initial-scale=1,
+          minimum-scale=1,
+          maximum-scale=1"/>
+</head>
+<body>
+
+<main>
+
 <?php
 $n = 4;
 
@@ -7,3 +25,12 @@ for ($i = 1; $i <= $n; $i++){
     }
 
 }
+
+?>
+
+</main>
+
+
+
+</body>
+</html>

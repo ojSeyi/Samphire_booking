e767ce6b4cd1bd1512e5d0cb188ca1334f8af0eb
ojSeyi <1509941@rgu.ac.uk>
2017-01-06 11:33:29 +0100

    Commit

--- test.php
+++ test.php
@@ -28,19 +28,6 @@ for ($n = $i; $n > 0; $n=$n-2){
     echo " <br> ";
 }
 
-$temperatures = [];
-$day1 = 22;
-
-for ($i=0; $i<=10; $i++){
-    $temperatures += $day1;
-    $day1 += 3;
-}
-$k=0;
-for ($i=0; $i <= count($temperatures); $i++){
-    echo $temperatures[$k];
-    echo "<br>";
-    $k++;
-}
 
 
 
